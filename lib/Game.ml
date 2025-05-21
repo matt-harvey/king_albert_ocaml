63f444248bece7aa504c45cd97882dd918352524
Matt Harvey <software@matthewharvey.net>
2025-05-22 07:41:58 +1000

    Minor tweaks

--- lib/Game.ml
+++ lib/Game.ml
@@ -1,56 +1,54 @@
-type t = {board: Board.t; mutable game_state: GameState.t}
+type t = Game of Board.t
 
-let make = {board= Deck.make_shuffled |> Board.from_deck; game_state= GameState.Prepared}
+let make : t = Game(Deck.make_shuffled |> Board.from_deck)
 
 let rec play (out_channel : out_channel) (in_channel : in_channel) (game : t) : unit =
-  game.game_state <- GameState.Playing ;
-  while game.game_state <> GameState.Quit do
-    Board.put out_channel game.board ;
-    consume_move out_channel in_channel game
+  let Game(board) = game in
+  Board.put out_channel board ;
+  while consume_move out_channel in_channel game <> GameState.Quit do
+    Board.put out_channel board ;
   done
 
-and consume_move (out_channel : out_channel) (in_channel : in_channel) (game : t) : unit =
+and consume_move (out_channel : out_channel) (in_channel : in_channel) (game : t) : GameState.t =
+  let put str = output_string out_channel str in
   output_string out_channel (AnsiColor.to_string AnsiColor.Green) ;
-  let board = game.board in
+  let Game(board) = game in
+  let game_state = ref GameState.Playing in
   let break_loop = ref false in
   while not !break_loop do
-    output_string out_channel "Enter your move: " ;
+    put "Enter your move: " ;
     Out_channel.flush out_channel ;
     let input = In_channel.input_line in_channel in
     match input with
     | None -> ()
     | Some input -> (
         if input = "quit" || input = "exit" then (
-          game.game_state <- GameState.Quit ;
+          game_state := GameState.Quit ;
           break_loop := true )
         else
           match Move.from_str input with
-          | Option.Some {origin; destination} ->
+          | Some {origin; destination} ->
               let origin_position = Board.position_at board origin in
               let destination_position = Board.position_at board destination in
               let origin_can_give = Position.can_give origin_position in
               if origin_can_give then
                 let updated_origin_position, card_given = origin_position |> Position.give in
                 match card_given with
-                | Option.Some card -> (
+                | Some card -> (
                     let destination_can_receive = Position.can_receive destination_position card in
                     match destination_can_receive with
                     | false ->
-                        output_string out_channel
-                          "Invalid move. Please enter your move (two letters) or 'quit' to quit." ;
-                        output_string out_channel "\n\n" ;
-                        ()
+                        put "Invalid move. Please enter your move (two letters) or 'quit' to quit.\n\n" ;
                     | true ->
-                        let updated_destionation_position = Position.receive destination_position card in
+                        let updated_destination_position = Position.receive destination_position card in
                         Board.update_position origin updated_origin_position board ;
-                        Board.update_position destination updated_destionation_position board ;
+                        Board.update_position destination updated_destination_position board ;
                         break_loop := true )
-                | Option.None -> ()
+                | None -> ()
               else ()
-          | Option.None ->
-              output_string out_channel
-                "Invalid move. Please enter your move (two letters) or 'quit' to quit." ;
-              output_string out_channel "\n\n" ;
+          | None ->
+              put "Invalid move. Please enter your move (two letters) or 'quit' to quit.\n\n" ;
               () ;
               () )
-  done
+  done;
+  !game_state
--- lib/Move.ml
+++ lib/Move.ml
@@ -1,33 +1,12 @@
 type t = {origin: int; destination: int}
 
-let char_to_pos_idx ch =
-  match ch with
-  | 'a' -> Option.Some 0
-  | 'b' -> Option.Some 1
-  | 'c' -> Option.Some 2
-  | 'd' -> Option.Some 3
-  | 'e' -> Option.Some 4
-  | 'f' -> Option.Some 5
-  | 'g' -> Option.Some 6
-  | 'h' -> Option.Some 7
-  | 'i' -> Option.Some 8
-  | 'j' -> Option.Some 9
-  | 'k' -> Option.Some 10
-  | 'l' -> Option.Some 11
-  | 'm' -> Option.Some 12
-  | 'n' -> Option.Some 13
-  | 'o' -> Option.Some 14
-  | 'p' -> Option.Some 15
-  | 'q' -> Option.Some 16
-  | 'r' -> Option.Some 17
-  | 's' -> Option.Some 18
-  | 't' -> Option.Some 19
-  | _ -> Option.None
+let char_to_position_index (ch : char) : int option =
+  if ch < 'a' || ch > 't' then None else Some(Char.code(ch) - Char.code('a'))
 
 let from_str (s : string) : t option =
-  match String.length s with
-  | 2 -> (
-    match (char_to_pos_idx s.[0], char_to_pos_idx s.[1]) with
-    | Option.Some origin, Option.Some destination -> Option.Some {origin; destination}
-    | _ -> Option.None )
-  | _ -> Option.None
+  if String.length s <> 2 then
+    None
+  else
+    match char_to_position_index s.[0], char_to_position_index s.[1] with
+    | Some origin, Some destination -> Some {origin; destination}
+    | _ -> None
